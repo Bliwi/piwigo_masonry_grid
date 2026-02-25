ec8dcf1453f92914b632299acf3300e2d80e7f9d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 08:52:06 +0100

    Update fr_FR, thanks to laurent.duretz

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -6,3 +6,15 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['Photos per page'] = 'Photos par page';
+$lang['Masonry grid'] = 'Grille';
+$lang['Thumbnail corner radius (px)'] = 'Rayon du coin (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'L\'espace entre les miniatures doit être supérieur ou égal à zéro';
+$lang['Thumbnails'] = 'Miniatures';
+$lang['Thumbnail width must be a positive integer'] = 'La taille de la miniature doit être supérieure à zéro';
+$lang['Thumbnail width (px)'] = 'Taille de la miniature (px)';
+$lang['Settings saved'] = 'Paramètres enregistrés';
+$lang['Save settings'] = 'Enregistrer les paramètres';
+$lang['Number of photos per page must be a positive integer'] = 'Le nombre de photos par page dot être supérieur à zéro';
+$lang['Gap between thumbnails (px)'] = 'Espace entre les miniatures (px)';
+$lang['Configure thumbnail width, gap and images per page.'] = 'Configurer la taille, l\'espace et le nombre de miniatures par page';
+$lang['Corner radius must be zero or a positive integer'] = 'Le rayon du coin doit être supérieur ou égal à zéro';
