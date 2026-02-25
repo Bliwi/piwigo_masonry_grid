6fc4de125a6272bbde7ce4c5125e8609792ae41d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 16:06:59 +0100

    Update tr_TR, thanks to katpatuka

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = 'Küçük resim genişliğini, aralığı ve sayfa başına resim sayısını yapılandırın.';
+$lang['Corner radius must be zero or a positive integer'] = 'Köşe yarıçapı sıfır veya pozitif bir tamsayı olmalıdır.';
+$lang['Gap between thumbnails (px)'] = 'Küçük resimler arasındaki boşluk (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'Küçük resimler arasındaki boşluk sıfır veya pozitif bir tamsayı olmalıdır.';
+$lang['Masonry grid'] = 'Duvarcılık ızgarası';
+$lang['Number of photos per page must be a positive integer'] = 'Sayfa başına fotoğraf sayısı pozitif bir tamsayı olmalıdır.';
+$lang['Photos per page'] = 'Sayfa başına fotoğraf sayısı';
+$lang['Save settings'] = 'Ayarları kaydet';
+$lang['Settings saved'] = 'Ayarlar kaydedildi';
+$lang['Thumbnail corner radius (px)'] = 'Küçük resim köşe yarıçapı (px)';
+$lang['Thumbnail width (px)'] = 'Küçük resim genişliği (px)';
+$lang['Thumbnail width must be a positive integer'] = 'Küçük resim genişliği pozitif bir tamsayı olmalıdır';
+$lang['Thumbnails'] = 'Küçük resimler';
