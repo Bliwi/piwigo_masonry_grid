76c0b558617a32a1b9a21cae0ae784ffbf3e0d8c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 10:20:30 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = 'Opsæt miniaturebilledes bredde, mellem og billeder pr. side';
+$lang['Corner radius must be zero or a positive integer'] = 'Hjørneradius skal være nul eller et positivt heltal';
+$lang['Gap between thumbnails (px)'] = 'Mellemrum mellem miniaturebilleder (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'Mellemrum mellem miniaturebilleder skal være nul eller et positivt heltal';
+$lang['Masonry grid'] = 'Murstensmønster';
+$lang['Number of photos per page must be a positive integer'] = 'Antal fotografier pr. side skal være et positivt heltal';
+$lang['Photos per page'] = 'Fotografier pr. side';
+$lang['Save settings'] = 'Gem indstillinger';
+$lang['Settings saved'] = 'Indstillinger gemt';
+$lang['Thumbnail corner radius (px)'] = 'Miniaturebilledes hjørneradius (rx)';
+$lang['Thumbnail width (px)'] = 'Miniaturebilledes bredde (px)';
+$lang['Thumbnail width must be a positive integer'] = 'Miniaturebilledes bredde skal være et positivt heltal';
+$lang['Thumbnails'] = 'Miniaturebilleder';
