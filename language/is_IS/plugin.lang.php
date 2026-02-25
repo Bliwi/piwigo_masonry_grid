8ac7cdfcd226ee284b6136092c0309b5b84d2727
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 14:02:53 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = 'Stilla breidd smámynda, bil og myndir á hverja síðu.';
+$lang['Corner radius must be zero or a positive integer'] = 'Hornradíus verður að vera núll eða jákvæð heiltala';
+$lang['Gap between thumbnails (px)'] = 'Bil á milli smámynda (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'Bilið á milli smámynda verður að vera núll eða jákvæð heiltala';
+$lang['Masonry grid'] = 'Múrsteinsskipulag';
+$lang['Number of photos per page must be a positive integer'] = 'Fjöldi ljósmynda á síðu verður að vera jákvæð heiltala';
+$lang['Photos per page'] = 'Myndir á síðu';
+$lang['Save settings'] = 'Vista stillingar';
+$lang['Settings saved'] = 'Stillingar vistaðar';
+$lang['Thumbnail corner radius (px)'] = 'Hornradíus smámyndar (px)';
+$lang['Thumbnail width (px)'] = 'Breidd smámyndar (px)';
+$lang['Thumbnail width must be a positive integer'] = 'Breidd smámyndar verður að vera jákvæð heiltala';
+$lang['Thumbnails'] = 'Smámyndir';
