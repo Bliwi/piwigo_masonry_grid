5d4f9dffdea26f971bd4a803178104bee17a6427
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 08:52:03 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = 'הגדר את רוחב התמונות הממוזערות, הרווח ותמונות לעמוד.';
+$lang['Corner radius must be zero or a positive integer'] = 'רדיוס הפינה חייב להיות אפס או מספר שלם חיובי';
+$lang['Gap between thumbnails (px)'] = 'רווח בין תמונות ממוזערות (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'רווח בין תמונות ממוזערות חייב להיות אפס או מספר שלם חיובי';
+$lang['Masonry grid'] = 'רשת Masonry';
+$lang['Number of photos per page must be a positive integer'] = 'מספר התמונות לעמוד חייב להיות מספר שלם חיובי';
+$lang['Photos per page'] = 'תמונות לעמוד';
+$lang['Save settings'] = 'שמור הגדרות';
+$lang['Settings saved'] = 'ההגדרות נשמרו';
+$lang['Thumbnail corner radius (px)'] = 'רדיוס פינת תמונה ממוזערת (px)';
+$lang['Thumbnail width (px)'] = 'רוחב תמונה ממוזערת (px)';
+$lang['Thumbnail width must be a positive integer'] = 'רוחב תמונה ממוזערת חייב להיות מספר שלם חיובי';
+$lang['Thumbnails'] = 'תמונות ממוזערות';
