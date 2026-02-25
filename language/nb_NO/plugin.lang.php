5310987d24d0b1b24fda69eebc5e8b289bee1f5d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 16:06:53 +0100

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = 'Konfigurer miniatyrbredde, mellomrom og bilder per side.';
+$lang['Corner radius must be zero or a positive integer'] = 'Hjørneradiusen må være null eller et positivt heltall';
+$lang['Gap between thumbnails (px)'] = 'Avstand mellom miniatyrbilder (piksler)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'Avstanden mellom miniatyrbildene må være null eller et positivt heltall';
+$lang['Masonry grid'] = 'Murnett';
+$lang['Number of photos per page must be a positive integer'] = 'Antall bilder per side må være et positivt heltall';
+$lang['Photos per page'] = 'Bilder per side';
+$lang['Save settings'] = 'Lagre innstillinger';
+$lang['Settings saved'] = 'Innstillingene er lagret';
+$lang['Thumbnail corner radius (px)'] = 'Hjørneradius for miniatyrbilde (piksler)';
+$lang['Thumbnail width (px)'] = 'Miniatyrbildebredde (piksler)';
+$lang['Thumbnail width must be a positive integer'] = 'Miniatyrbildebredden må være et positivt heltall';
+$lang['Thumbnails'] = 'Miniatyrbilder';
