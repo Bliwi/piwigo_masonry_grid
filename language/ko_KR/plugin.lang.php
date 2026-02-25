e6767aa1bce6c6207fdd97a026bfeedaf036a110
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-25 09:01:14 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = '썸네일 너비, 간격 및 페이지당 이미지 수 설정';
+$lang['Corner radius must be zero or a positive integer'] = '모서리 반경은 0 또는 양의 정수여야 합니다';
+$lang['Gap between thumbnails (px)'] = '썸네일 간격 (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = '썸네일 간격은 0 또는 양의 정수여야 합니다';
+$lang['Masonry grid'] = '벽돌 배열 그리드';
+$lang['Number of photos per page must be a positive integer'] = '페이지당 사진 수는 양의 정수여야 합니다';
+$lang['Photos per page'] = '페이지당 사진 수';
+$lang['Save settings'] = '설정 저장';
+$lang['Settings saved'] = '설정 저장됨';
+$lang['Thumbnail corner radius (px)'] = '썸네일 모서리 반경 (px)';
+$lang['Thumbnail width (px)'] = '썸네일 너비 (px)';
+$lang['Thumbnail width must be a positive integer'] = '썸네일 너비는 양의 정수여야 합니다';
+$lang['Thumbnails'] = '썸네일';
