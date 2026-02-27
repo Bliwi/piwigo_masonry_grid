5238d879771f81415a16c418ef2cce3e6608ceda
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-27 04:02:49 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = 'Configureu l\'amplada, l\'espai i les imatges per pàgina de les miniatures.';
+$lang['Corner radius must be zero or a positive integer'] = 'El radi de la cantonada ha de ser zero o un número enter positiu';
+$lang['Gap between thumbnails (px)'] = 'Espai entre miniatures (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'L\'espai entre miniatures ha de ser zero o un número enter positiu';
+$lang['Masonry grid'] = 'Graella de maçoneria';
+$lang['Number of photos per page must be a positive integer'] = 'El nombre de fotos per pàgina ha de ser un número enter positiu';
+$lang['Photos per page'] = 'Fotos per pàgina';
+$lang['Save settings'] = 'Desa els paràmetres';
+$lang['Settings saved'] = 'Paràmetres desats';
+$lang['Thumbnail corner radius (px)'] = 'Radi de la cantonada de la miniatura (px)';
+$lang['Thumbnail width (px)'] = 'Amplada de la miniatura (px)';
+$lang['Thumbnail width must be a positive integer'] = 'L\'amplada de la miniatura ha de ser un número enter positiu';
+$lang['Thumbnails'] = 'Miniatures';
