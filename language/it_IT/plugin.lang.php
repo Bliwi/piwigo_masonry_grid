210fe96e63e4ad87118f9c4095989f320b0d7c60
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-03 12:35:35 +0100

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = 'Configura larghezza miniatura, gap ed immagini per pagina.';
+$lang['Corner radius must be zero or a positive integer'] = 'Il raggio dell\'angolo deve essere zero o un numero intero positivo';
+$lang['Gap between thumbnails (px)'] = 'Gap tra le miniature (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'Gap tra le miniature deve essere zero o un numero intero positivo';
+$lang['Masonry grid'] = 'Griglia sfalsata';
+$lang['Number of photos per page must be a positive integer'] = 'Il numero di foto per pagina deve essere un numero intero positivo';
+$lang['Photos per page'] = 'Fotografie per pagina';
+$lang['Save settings'] = 'Salva impostazioni';
+$lang['Settings saved'] = 'Impostazioni salvate';
+$lang['Thumbnail corner radius (px)'] = 'Raggio dell\'angolo della miniatura (px)';
+$lang['Thumbnail width (px)'] = 'Larghezza miniatura (px)';
+$lang['Thumbnail width must be a positive integer'] = 'La larghezza della miniatura deve essere un numero intero positivo';
+$lang['Thumbnails'] = 'Miniature';
