c6e760d827630fb1762e3a29621ceefc1b1960f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-05 06:22:14 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Configure thumbnail width, gap and images per page.'] = 'Konfigurera miniatyrernas bredd, mellanrum och bilder per sida.';
+$lang['Corner radius must be zero or a positive integer'] = 'Hörnradien måste vara noll eller ett positivt heltal';
+$lang['Gap between thumbnails (px)'] = 'Mellanrum mellan miniatyrer (px)';
+$lang['Gap between thumbnails must be zero or a positive integer'] = 'Mellanrum mellan miniatyrer måste var noll eller positivt heltal';
+$lang['Masonry grid'] = 'Rutnät';
+$lang['Number of photos per page must be a positive integer'] = 'Antal foton per sida måste vara positivt heltal';
+$lang['Photos per page'] = 'Foton per sida';
+$lang['Save settings'] = 'Spara inställningar';
+$lang['Settings saved'] = 'Inställningar sparade';
+$lang['Thumbnail corner radius (px)'] = 'Hörnradie miniatyr (px)';
+$lang['Thumbnail width (px)'] = 'Miniatyrbredd (px)';
+$lang['Thumbnail width must be a positive integer'] = 'Miniatyrbredd måste vara positivt heltal';
+$lang['Thumbnails'] = 'Miniatyrer';
